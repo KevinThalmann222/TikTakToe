60bf8290687318991df8be106b723ed772ad2bff
Kevin Thalmann <96835756+KevinThalmann222@users.noreply.github.com>
2022-09-23 08:18:54 +0200

    differentiation between player x and y
    Player X
    &
    Player Y

--- TikTakToe.cc
+++ TikTakToe.cc
@@ -5,47 +5,54 @@ using namespace std;
 // Spielfeld mit der groeße 4x4
 char playing_field[4][4] = {{'A', 'B', 'C', 'D'}, {'E', 'F', 'G', 'H'}, {'I', 'J', 'K', 'L'}, {'M', 'N', 'O', 'P'}};
 char get_pos;
-char set_char = 'X';
+char player01 = 'X';
+char player02 = 'Y';
 
 
 char WinningPlayer()
 {
     // Gamingen Player X
-    if (playing_field[0][0] == 'X' && playing_field[0][1] == 'X' && playing_field[0][2] == 'X' &&
-        playing_field[0][3] == 'X')
-        return 'X';
-    else if (playing_field[1][0] == 'X' && playing_field[1][1] == 'X' && playing_field[1][2] == 'X' &&
-             playing_field[1][3] == 'X')
-        return 'X';
-    else if (playing_field[2][0] == 'X' && playing_field[2][1] == 'X' && playing_field[2][2] == 'X' &&
-             playing_field[3][3] == 'X')
-        return 'X';
-    else if (playing_field[4][0] == 'X' && playing_field[4][1] == 'X' && playing_field[4][2] == 'X' &&
-             playing_field[4][3] == 'X')
-        return 'X';
+    if (playing_field[0][0] == player01 && playing_field[0][1] == player01 && playing_field[0][2] == player01 &&
+        playing_field[0][3] == player01)
+        return player01;
+    else if (playing_field[1][0] == player01 && playing_field[1][1] == player01 && playing_field[1][2] == player01 &&
+             playing_field[1][3] == player01)
+        return player01;
+    else if (playing_field[2][0] == player01 && playing_field[2][1] == player01 && playing_field[2][2] == player01 &&
+             playing_field[3][3] == player01)
+        return player01;
+    else if (playing_field[4][0] == player01 && playing_field[4][1] == player01 && playing_field[4][2] == player01 &&
+             playing_field[4][3] == player01)
+        return player01;
     // Gamingen Player Y
-    else if (playing_field[0][0] == 'Y' && playing_field[0][1] == 'Y' && playing_field[0][2] == 'Y' &&
-             playing_field[0][3] == 'Y')
-        return 'Y';
-    else if (playing_field[1][0] == 'Y' && playing_field[1][1] == 'Y' && playing_field[1][2] == 'Y' &&
-             playing_field[1][3] == 'Y')
-        return 'Y';
-    else if (playing_field[2][0] == 'Y' && playing_field[2][1] == 'Y' && playing_field[2][2] == 'Y' &&
-             playing_field[3][3] == 'Y')
-        return 'Y';
-    else if (playing_field[4][0] == 'Y' && playing_field[4][1] == 'Y' && playing_field[4][2] == 'Y' &&
-             playing_field[4][3] == 'Y')
-        return 'Y';
+    else if (playing_field[0][0] == player02 && playing_field[0][1] == player02 && playing_field[0][2] == player02 &&
+             playing_field[0][3] == player02)
+        return player02;
+    else if (playing_field[1][0] == player02 && playing_field[1][1] == player02 && playing_field[1][2] == player02 &&
+             playing_field[1][3] == player02)
+        return player02;
+    else if (playing_field[2][0] == player02 && playing_field[2][1] == player02 && playing_field[2][2] == player02 &&
+             playing_field[3][3] == player02)
+        return player02;
+    else if (playing_field[4][0] == player02 && playing_field[4][1] == player02 && playing_field[4][2] == player02 &&
+             playing_field[4][3] == player02)
+        return player02;
     else
         return '/';
 }
 
 void SwitchPlayer()
 {
-    if (set_char == 'X')
-        set_char = 'Y';
+    if (player01 == 'X')
+    {
+        player01 = 'Y';
+        player02 = 'X';
+    }
     else
-        set_char = 'X';
+    {
+        player01 = 'X';
+        player02 = 'Y';
+    }
 }
 
 void DrawField()
@@ -63,57 +70,57 @@ void DrawField()
 
 void DrawMove()
 {
-    cout << "Bitte wähle ein Feld aus [Player: " << set_char << "]: ";
+    cout << "Bitte wähle ein Feld aus [Player: " << player01 << "]: ";
     cin >> get_pos;
     switch (get_pos)
     {
     case 'a':
-        playing_field[0][0] = set_char;
+        playing_field[0][0] = player01;
         break;
     case 'b':
-        playing_field[0][1] = set_char;
+        playing_field[0][1] = player01;
         break;
     case 'c':
-        playing_field[0][2] = set_char;
+        playing_field[0][2] = player01;
         break;
     case 'd':
-        playing_field[0][3] = set_char;
+        playing_field[0][3] = player01;
         break;
     case 'e':
-        playing_field[1][0] = set_char;
+        playing_field[1][0] = player01;
         break;
     case 'f':
-        playing_field[1][1] = set_char;
+        playing_field[1][1] = player01;
         break;
     case 'g':
-        playing_field[1][2] = set_char;
+        playing_field[1][2] = player01;
         break;
     case 'h':
-        playing_field[1][3] = set_char;
+        playing_field[1][3] = player01;
         break;
     case 'i':
-        playing_field[2][0] = set_char;
+        playing_field[2][0] = player01;
         break;
     case 'j':
-        playing_field[2][1] = set_char;
+        playing_field[2][1] = player01;
         break;
     case 'k':
-        playing_field[2][2] = set_char;
+        playing_field[2][2] = player01;
         break;
     case 'l':
-        playing_field[2][3] = set_char;
+        playing_field[2][3] = player01;
         break;
     case 'm':
-        playing_field[3][0] = set_char;
+        playing_field[3][0] = player01;
         break;
     case 'n':
-        playing_field[3][1] = set_char;
+        playing_field[3][1] = player01;
         break;
     case 'o':
-        playing_field[3][2] = set_char;
+        playing_field[3][2] = player01;
         break;
     case 'p':
-        playing_field[3][3] = set_char;
+        playing_field[3][3] = player01;
         break;
     }
 }
